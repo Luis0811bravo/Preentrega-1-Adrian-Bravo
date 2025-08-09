f85a79b425ca0a5af8aa6434423511586c6959b0
Adrian Bravo <adrian081191@gmail.com>
2025-08-08 19:54:06 -0600

    Actualizacion preentrega 1
    Actualización de preentrega 1

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-let nombre = prompt('Bienvenido a Sciencie2everyone, favor de ingrsar su nombre');
-
-console.log(nombre)
-
-alert(`Buenos dias ${nombre}. ¿Listo para aprender algo nuevo hoy?`)
-
+let usuariosRegistrados = [
+    'Adrian Bravo',
+    'Luis Anaya',
+    'Sergio Peña'
+];
 
 const cursos = [
     'algebra',
@@ -13,9 +12,42 @@ const cursos = [
     'quimica'
 ];
 
+function saludar( usuario ) {
+    alert(`Bienvenido ${usuario}. . ¿Listo para aprender algo nuevo hoy?`)
+}
+
+function ingresarPortal() {
+   
+    let existe = false;
+
+    for (let i = 0; i < usuariosRegistrados.length; i++) {
+        if (usuariosRegistrados[i] === usuario) {
+            existe = true;
+            break;
+        }
+    }
+
+    if (encontrado) {
+        saludar(usuario);
+    } else {
+        alert("Usuario no registrado.");
+        return;
+    }
+}
+
+let usuario = prompt('Bienvenido a Sciencie2everyone, favor de ingrsar su nombre registrado');
+
+while(!usuario) {
+usuario = prompt('Por favor ingresa tu usuario');
+// Cómo salir cuando le dan cancelar en el prompt????
+}
+
+ingresarPortal();
+
+
 let cursoSeleccionado = prompt('Selecciona de entre los siguientes cursos disponibles: Algebra, Calculo, Trigonometria, Fisica, Quimica');
 cursoSeleccionado.toLowerCase();
-console.log(cursoSeleccionado);
+// console.log(cursoSeleccionado);
 
 while(!cursoSeleccionado) {
     cursoSeleccionado = prompt('Favor de seleccionar alguno de los cursos disponibles: Algebra, Calculo, Trigonometria, Fisica, Quimica');
